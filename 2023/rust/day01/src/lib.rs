de79c14ee475dc42ec22e07f08d5099969082305
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-01 12:32:04 +0545

    Complete 01-02
    Get real calibration value (parse spelled out nums)

--- 2023/rust/day01/src/lib.rs
+++ 2023/rust/day01/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn get_calibration_value(input: &str) -> u64 {
     let nums = input
         .chars()
@@ -11,43 +13,130 @@ pub fn get_calibration_value(input: &str) -> u64 {
         .expect("always a valid number")
 }
 
+pub fn get_real_calibration_value(input: &str) -> u64 {
+    let spelled_num_map: HashMap<&str, char> = HashMap::from([
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ]);
+
+    let mut nums: Vec<char> = vec![];
+    let input_len = input.len();
+
+    let mut skip = 0;
+    while skip < input_len {
+        let rest = input.chars().skip(skip).collect::<String>();
+
+        let first_char = rest
+            .chars()
+            .next()
+            .expect("should contain at least one char");
+
+        if first_char.is_ascii_digit() {
+            nums.push(first_char);
+        }
+        // check spelled out numerals
+        // needs at least three chars viz., one, two, six)
+        else if rest.len() > 2 {
+            for spelled_numeral in spelled_num_map.keys() {
+                if rest.starts_with(spelled_numeral) {
+                    nums.push(spelled_num_map[spelled_numeral]);
+                    break;
+                }
+            }
+        }
+
+        skip += 1;
+    }
+
+    format!("{}{}", nums[0], nums[nums.len() - 1])
+        .parse::<u64>()
+        .expect("should always be an int")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase<'a> {
+        input: &'a str,
+        expected: u64,
+    }
+
     #[test]
     fn gets_correct_calibration_val() {
-        struct TestCase<'a> {
-            input: &'a str,
-            expected_output: u64,
-        }
-
         let test_cases: Vec<TestCase> = vec![
             TestCase {
                 input: "1abc2",
-                expected_output: 12,
+                expected: 12,
             },
             TestCase {
                 input: "pqr3stu8vwx",
-                expected_output: 38,
+                expected: 38,
             },
             TestCase {
                 input: "a1b2c3d4e5f",
-                expected_output: 15,
+                expected: 15,
             },
             TestCase {
                 input: "treb7uchet",
-                expected_output: 77,
+                expected: 77,
             },
         ];
 
-        for TestCase {
-            input,
-            expected_output,
-        } in test_cases
-        {
-            let result = get_calibration_value(input);
-            assert_eq!(result, expected_output);
+        for TestCase { input, expected } in test_cases {
+            let actual = get_calibration_value(input);
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn gets_correct_real_calibration_val() {
+        let test_cases: Vec<TestCase> = vec![
+            TestCase {
+                input: "two1nine",
+                expected: 29,
+            },
+            TestCase {
+                input: "eightwothree",
+                expected: 83,
+            },
+            TestCase {
+                input: "abcone2threexyz",
+                expected: 13,
+            },
+            TestCase {
+                input: "xtwone3four",
+                expected: 24,
+            },
+            TestCase {
+                input: "4nineeightseven2",
+                expected: 42,
+            },
+            TestCase {
+                input: "zoneight234",
+                expected: 14,
+            },
+            TestCase {
+                input: "7pqrstsixteen",
+                expected: 76,
+            },
+            // personal test case; overlapping numerals
+            TestCase {
+                input: "threetwone",
+                expected: 31,
+            },
+        ];
+
+        for TestCase { input, expected } in test_cases {
+            let actual = get_real_calibration_value(input);
+            assert_eq!(actual, expected, "{}", input);
         }
     }
 }
--- 2023/rust/day01/src/main.rs
+++ 2023/rust/day01/src/main.rs
@@ -1,16 +1,18 @@
-use std::fs;
+use std::{fs, u64};
 
-use day01::get_calibration_value;
+use day01::{get_calibration_value, get_real_calibration_value};
 
-const INPUT_LOCATION: &str = "input/values.txt";
+const INPUT_DIR: &str = "input";
 
 fn main() {
-    let full_input = fs::read_to_string(INPUT_LOCATION).expect("input file must be readable");
-
-    println!("Sum of calibration values = {}", part_01(&full_input));
+    part_01();
+    part_02();
 }
 
-fn part_01(full_input: &str) -> u64 {
+fn part_01() {
+    let full_input = fs::read_to_string(format!("{INPUT_DIR}/values.txt"))
+        .expect("input file values.txt must be readable");
+
     let lines = full_input.lines();
 
     let mut sum: u64 = 0;
@@ -18,5 +20,19 @@ fn part_01(full_input: &str) -> u64 {
         sum += get_calibration_value(line);
     }
 
-    sum
+    println!("Sum of calibration values = {}", sum);
+}
+
+fn part_02() {
+    let full_input = fs::read_to_string(format!("{INPUT_DIR}/real_values.txt"))
+        .expect("input file: real_values.txt must be readable");
+
+    let lines = full_input.lines();
+
+    let mut sum: u64 = 0;
+    for line in lines {
+        sum += get_real_calibration_value(line);
+    }
+
+    println!("Sum of real calibration values = {}", sum);
 }
