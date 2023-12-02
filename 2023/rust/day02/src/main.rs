b15a247f94377d0e6f2a1af6308dd3fa30d9c204
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-03 01:16:07 +0545

    Complete 02-02

--- 2023/rust/day02/src/main.rs
+++ 2023/rust/day02/src/main.rs
@@ -6,6 +6,7 @@ const INPUT_DIR: &str = "input";
 
 fn main() {
     part_01(&format!("{INPUT_DIR}/part_one_values.txt"));
+    part_02(&format!("{INPUT_DIR}/part_two_values.txt"));
 }
 
 fn part_01(input_path: &str) {
@@ -33,4 +34,47 @@ fn part_01(input_path: &str) {
     }
 
     println!("Sum of possible games = {sum}");
+}
+
+fn part_02(input_path: &str) {
+    let contents = fs::read_to_string(input_path).expect("file must be readable");
+
+    let mut sum = 0;
+    for line in contents.lines() {
+        let (_game_num, games) = get_game(line);
+
+        let mut min_cube_count = HashMap::from([(Cube::Red, 0), (Cube::Blue, 0), (Cube::Green, 0)]);
+        for game in games.split("; ") {
+            let cube_count_map = get_cube_count(game);
+
+            if cube_count_map
+                .get(&Cube::Red)
+                .unwrap_or(&0)
+                .gt(&min_cube_count[&Cube::Red])
+            {
+                min_cube_count.insert(Cube::Red, cube_count_map[&Cube::Red]);
+            }
+
+            if cube_count_map
+                .get(&Cube::Blue)
+                .unwrap_or(&0)
+                .gt(&min_cube_count[&Cube::Blue])
+            {
+                min_cube_count.insert(Cube::Blue, cube_count_map[&Cube::Blue]);
+            }
+
+            if cube_count_map
+                .get(&Cube::Green)
+                .unwrap_or(&0)
+                .gt(&min_cube_count[&Cube::Green])
+            {
+                min_cube_count.insert(Cube::Green, cube_count_map[&Cube::Green]);
+            }
+        }
+        let power =
+            min_cube_count[&Cube::Red] * min_cube_count[&Cube::Blue] * min_cube_count[&Cube::Green];
+        sum += power;
+    }
+
+    println!("Sum of power of min possible games = {sum}");
 }
