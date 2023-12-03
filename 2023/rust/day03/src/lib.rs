cbc7c04aeb7c880a60d6a3af0bf0c388008a13b4
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-04 01:11:48 +0545

    Complete 03-02

--- 2023/rust/day03/src/lib.rs
+++ 2023/rust/day03/src/lib.rs
@@ -109,8 +109,112 @@ fn contains_special_chars(adjacent_vals: [char; 8]) -> bool {
     true
 }
 
+#[derive(Debug, PartialEq)]
+struct NumberSpan {
+    start: usize,
+    end: usize,
+}
+
+#[derive(Debug, PartialEq)]
+struct NumInRow {
+    num: usize,
+    span: NumberSpan,
+}
+
+pub fn compute_total_gear_ratio(matrix: Vec<&str>) -> usize {
+    const GEAR: char = '*';
+    const REQUIRED_ADJACENT_NUMS: usize = 2;
+
+    let mut sum = 0;
+    for (row, line) in matrix.iter().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch != GEAR {
+                continue;
+            }
+
+            let adjacent_nums = find_adjacent_nums((row, col), &matrix);
+            if adjacent_nums.len() == REQUIRED_ADJACENT_NUMS {
+                sum += adjacent_nums[0] * adjacent_nums[1];
+            }
+        }
+    }
+
+    sum
+}
+
+fn find_nums_in_row(row: &str) -> Vec<NumInRow> {
+    let mut running_num = String::new();
+    let mut nums_in_row = vec![];
+
+    let mut start = 0;
+    for (i, ch) in row.chars().enumerate() {
+        if ch.is_ascii_digit() {
+            if running_num.is_empty() {
+                start = i;
+            }
+            running_num.push(ch);
+        } else if !running_num.is_empty() {
+            nums_in_row.push(NumInRow {
+                num: running_num
+                    .parse::<usize>()
+                    .expect("must be a valid number"),
+                span: NumberSpan { start, end: i - 1 },
+            });
+
+            // reset
+            running_num = String::new();
+        }
+    }
+
+    // check again at the end of row
+    if !running_num.is_empty() {
+        nums_in_row.push(NumInRow {
+            num: running_num
+                .parse::<usize>()
+                .expect("must be a valid number"),
+            span: NumberSpan {
+                start,
+                end: row.len() - 1,
+            },
+        });
+    }
+
+    nums_in_row
+}
+
+fn find_adjacent_nums((row, col): (usize, usize), matrix: &Vec<&str>) -> Vec<usize> {
+    let previous_row = row.saturating_sub(1);
+
+    let max_row = matrix.len();
+    let next_row = if row + 1 < max_row {
+        row + 1
+    } else {
+        max_row - 1
+    };
+
+    let mut nums_in_adjacent_rows = find_nums_in_row(matrix[previous_row]);
+    nums_in_adjacent_rows.append(&mut find_nums_in_row(matrix[row])); // current
+    nums_in_adjacent_rows.append(&mut find_nums_in_row(matrix[next_row]));
+
+    nums_in_adjacent_rows
+        .iter()
+        .filter_map(|num_in_row| {
+            // -1/+1 adjust for diagonals and immediate left/right
+            if (num_in_row.span.start.saturating_sub(1)) <= col && col <= (num_in_row.span.end + 1)
+            {
+                Some(num_in_row.num)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<usize>>()
+}
+
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+    use std::iter::zip;
+
     use super::*;
 
     #[test]
@@ -127,5 +231,32 @@ mod tests {
 .664.598..";
         let matrix = input.lines().collect::<Vec<&str>>();
         assert_eq!(compute_parts_sum(matrix), 4361);
+    }
+
+    #[rstest]
+    #[case("..123..#45/..12", vec![NumInRow { num: 123, span: NumberSpan { start: 2, end: 4 }}, NumInRow { num: 45, span: NumberSpan { start: 8, end: 9 }}, NumInRow { num: 12, span: NumberSpan { start: 13, end: 14 }}])]
+    fn finds_nums_in_row_correctly(#[case] input: &str, #[case] expected: Vec<NumInRow>) {
+        let actual_nums_in_row = find_nums_in_row(input);
+        assert_eq!(actual_nums_in_row.len(), expected.len());
+
+        for (actual_val, expected_val) in zip(actual_nums_in_row, expected) {
+            assert_eq!(actual_val, expected_val);
+        }
+    }
+
+    #[test]
+    fn computes_adjacent_nums_correctly() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        let matrix = input.lines().collect::<Vec<&str>>();
+        assert_eq!(find_adjacent_nums((1, 3), &matrix), vec![467, 35]);
     }
 }
--- 2023/rust/day03/src/main.rs
+++ 2023/rust/day03/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-use day03::compute_parts_sum;
+use day03::{compute_parts_sum, compute_total_gear_ratio};
 
 const INPUT_DIR: &str = "input";
 
 fn main() {
     part_01(&format!("{INPUT_DIR}/part_one_visual.txt"));
-    // part_01(&format!("{INPUT_DIR}/part_one_test.txt"));
+    part_02(&format!("{INPUT_DIR}/part_two_visual.txt"));
 }
 
 fn part_01(path: &str) {
@@ -16,4 +16,13 @@ fn part_01(path: &str) {
     let sum = compute_parts_sum(matrix);
 
     println!("Sum of parts = {sum}");
+}
+
+fn part_02(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+    let matrix = contents.lines().collect::<Vec<&str>>();
+
+    let total = compute_total_gear_ratio(matrix);
+
+    println!("Total gear ratio = {total}");
 }
