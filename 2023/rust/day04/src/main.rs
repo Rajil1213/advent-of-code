fc53c08308d39c3706961e7364f020a9107bc78d
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-04 12:47:13 +0545

    refactor: use consistent naming

--- 2023/rust/day04/src/main.rs
+++ 2023/rust/day04/src/main.rs
@@ -5,11 +5,11 @@ use day04::{calculate_points, calculate_total_cards, parse};
 const INPUT_DIR: &str = "input";
 
 fn main() {
-    part_01(&format!("{INPUT_DIR}/part_one_input.txt"));
+    part_one(&format!("{INPUT_DIR}/part_one_input.txt"));
     part_two(&format!("{INPUT_DIR}/part_two_input.txt"));
 }
 
-fn part_01(path: &str) {
+fn part_one(path: &str) {
     let contents = fs::read_to_string(path).expect("input file must be readable");
 
     let mut sum = 0;
