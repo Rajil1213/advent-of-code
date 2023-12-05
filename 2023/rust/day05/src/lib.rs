b3d8617df5f55b0592bfac864dfe7b4f0064720a
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-05 23:12:12 +0545

    Complete 05-01

---
+++ 2023/rust/day05/src/lib.rs
@@ -0,0 +1,146 @@
+const NUM_MAPS: i8 = 7;
+
+#[derive(Debug, Clone, Copy)]
+pub struct CategoryMap {
+    pub src: usize,
+    pub dst: usize,
+    pub range: usize,
+}
+
+impl CategoryMap {
+    pub fn get_value(&self, val: usize) -> Option<usize> {
+        if val < self.src {
+            return None;
+        }
+
+        let diff: usize = val - self.src;
+        if diff < self.range {
+            return Some(self.dst + diff);
+        }
+
+        None
+    }
+}
+
+pub fn get_seeds(line: &str) -> Vec<usize> {
+    line.split("seeds: ")
+        .last()
+        .expect("line must start with seeds:")
+        .split(' ')
+        .map(|v| v.parse::<usize>().expect("each seed must be a number"))
+        .collect::<Vec<usize>>()
+}
+
+pub fn find_mapped_value(maps: &[Vec<CategoryMap>; NUM_MAPS as usize], seed: usize) -> usize {
+    let mut value: usize = seed;
+    for map in maps {
+        value = map
+            .iter()
+            .find_map(|cm| cm.get_value(value))
+            .unwrap_or(value);
+    }
+
+    value
+}
+
+pub fn parse_map_lines(lines: Vec<&str>) -> [Vec<CategoryMap>; NUM_MAPS as usize] {
+    const CATEGORY_MAP_LIST: Vec<CategoryMap> = Vec::new();
+    let mut maps: [Vec<CategoryMap>; NUM_MAPS as usize] = [CATEGORY_MAP_LIST; NUM_MAPS as usize];
+
+    let mut map_index: i8 = -1; // so that first occurrence of "map:" causes index to be 1
+    for line in lines {
+        if line.ends_with("map:") {
+            // if line ends with ":map", start a new map
+            map_index += 1;
+            if map_index.gt(&NUM_MAPS) {
+                panic!("More than 8 maps found!");
+            }
+            continue;
+        }
+
+        if line.is_empty() {
+            continue;
+        }
+
+        // if line does not begin with a number
+        if !line.chars().next().unwrap().is_ascii_digit() {
+            continue;
+        }
+
+        maps[map_index as usize].push(get_map(line));
+    }
+
+    maps
+}
+
+pub fn get_map(line: &str) -> CategoryMap {
+    let values = line
+        .split(' ')
+        .map(|v| v.parse::<usize>().expect("each value must be a number"))
+        .collect::<Vec<usize>>();
+    assert!(values.len().eq(&3), "each line must have three values");
+
+    let dst = values[0];
+    let src = values[1];
+    let range = values[2];
+
+    CategoryMap { src, dst, range }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn computes_nearest_location_correctly() {
+        let contents = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+        let first_line = contents.lines().next().unwrap();
+        let full_input = contents.lines().collect::<Vec<&str>>();
+
+        let seeds = get_seeds(first_line);
+        let maps = parse_map_lines(full_input);
+
+        let mut min_location = usize::MAX;
+
+        for seed in seeds {
+            let location = find_mapped_value(&maps, seed);
+            if location.lt(&min_location) {
+                min_location = location;
+            }
+        }
+
+        assert_eq!(min_location, 35);
+    }
+}
---
+++ 2023/rust/day05/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+use day05::{find_mapped_value, get_seeds, parse_map_lines};
+
+const INPUT_DIR: &str = "input";
+
+fn main() {
+    part_one(&format!("{INPUT_DIR}/part_one.txt"));
+}
+
+fn part_one(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+    let first_line = contents
+        .lines()
+        .next()
+        .expect("input should contain at least one line");
+
+    let entire_input = contents.lines().collect::<Vec<&str>>();
+
+    let seeds = get_seeds(first_line);
+    let maps = parse_map_lines(entire_input);
+
+    let mut min_location = usize::MAX;
+
+    for seed in seeds {
+        let location = find_mapped_value(&maps, seed);
+        if location.lt(&min_location) {
+            min_location = location
+        }
+    }
+
+    println!("The nearest location is {min_location}");
+}
