bbfd73af737fbd768242647ad03b6829ec605aea
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-07 00:58:12 +0545

    Complete 06

---
+++ 2023/rust/day06/src/lib.rs
@@ -0,0 +1,55 @@
+pub fn ways_to_win(distance_to_beat: usize, total_race_time: usize) -> usize {
+    let min_charging_time = get_min_charging_time(distance_to_beat, total_race_time)
+        .expect("min charging time should exist");
+    let max_charging_time = get_max_charging_time(distance_to_beat, total_race_time)
+        .expect("max charging time should exist");
+
+    assert!(max_charging_time.ge(&min_charging_time));
+    (min_charging_time..=max_charging_time)
+        .collect::<Vec<usize>>()
+        .len()
+}
+
+fn get_min_charging_time(distance_to_beat: usize, total_race_time: usize) -> Option<usize> {
+    for i in 1..total_race_time {
+        let distance = total_distance(i, total_race_time);
+
+        if distance > distance_to_beat {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn get_max_charging_time(time_to_beat: usize, total_race_time: usize) -> Option<usize> {
+    for i in (1..total_race_time).rev() {
+        let distance = total_distance(i, total_race_time);
+
+        if distance > time_to_beat {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn total_distance(charging_time: usize, total_race_time: usize) -> usize {
+    let remaining_time = total_race_time - charging_time;
+
+    remaining_time * charging_time // charging_time = speed
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case((7, 9), 4)]
+    #[case((15, 40), 8)]
+    #[case((30, 200), 9)]
+    fn calculates_ways_to_win_correctly(#[case] input: (usize, usize), #[case] expected: usize) {
+        assert_eq!(ways_to_win(input.1, input.0), expected);
+    }
+}
---
+++ 2023/rust/day06/src/main.rs
@@ -0,0 +1,84 @@
+use std::{fs, iter::zip};
+
+use day06::ways_to_win;
+
+const INPUT_DIR: &str = "input";
+
+fn main() {
+    part_one(&format!("{INPUT_DIR}/part_one.txt"));
+    part_two(&format!("{INPUT_DIR}/part_two.txt"));
+}
+
+fn part_one(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+
+    let lines = contents.lines().collect::<Vec<&str>>();
+    assert!(lines.len().eq(&2), "file must have exactly two lines");
+
+    let time_line = lines[0]
+        .split(':')
+        .last()
+        .expect("first line must contain a colon")
+        .split(' ')
+        .filter_map(|v| {
+            if v.trim().is_empty() {
+                return None;
+            }
+            Some(v.parse::<usize>().expect("must be a number"))
+        })
+        .collect::<Vec<usize>>();
+
+    let distance_line = lines[1]
+        .split(':')
+        .last()
+        .expect("first line must contain a colon")
+        .split(' ')
+        .filter_map(|v| {
+            if v.trim().is_empty() {
+                return None;
+            }
+            Some(v.parse::<usize>().expect("must be a number"))
+        })
+        .collect::<Vec<usize>>();
+
+    assert!(time_line.len().eq(&distance_line.len()));
+
+    let mut product = 1;
+    for (total_race_time, distance_to_beat) in zip(time_line, distance_line) {
+        let num_ways_to_win = ways_to_win(distance_to_beat, total_race_time);
+        product *= num_ways_to_win;
+    }
+
+    println!("Product of Ways to win = {product}");
+}
+
+fn part_two(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+
+    let lines = contents.lines().collect::<Vec<&str>>();
+    assert!(lines.len().eq(&2), "file must have exactly two lines");
+
+    let total_race_time = lines[0]
+        .split(':')
+        .last()
+        .expect("first line must contain a colon")
+        .split(' ')
+        .filter(|v| !v.is_empty())
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("must be a vali number");
+
+    let distance_to_beat = lines[1]
+        .split(':')
+        .last()
+        .expect("first line must contain a colon")
+        .split(' ')
+        .filter(|v| !v.is_empty())
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("must be a number");
+
+    let num_ways_to_win = ways_to_win(distance_to_beat, total_race_time);
+
+    println!("Total Number of Ways to win = {num_ways_to_win}");
+}
