2e37c55963557a03e8e5217b727b05c5c1afe0ee
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-11 13:43:01 +0545

    Complete 11-02

--- 2023/rust/day11/src/lib.rs
+++ 2023/rust/day11/src/lib.rs
@@ -1,5 +1,3 @@
-use std::usize;
-
 const GALAXY: char = '#';
 const SPACE: char = '.';
 
@@ -13,7 +11,7 @@ pub fn load_image(input: &str) -> Vec<&str> {
     galaxy
 }
 
-pub fn expand_space(image: &[&str]) -> Vec<String> {
+pub fn expand_space(image: &[&str], factor: usize) -> Vec<String> {
     let mut transposed_image: Vec<String> = vec![];
 
     let num_col = image
@@ -41,20 +39,60 @@ pub fn expand_space(image: &[&str]) -> Vec<String> {
             expanded_row.push(chr);
             // push again if the col only has `.`
             if col.chars().all(|c| c == SPACE) {
-                expanded_row.push(chr);
+                for _ in 0..factor {
+                    expanded_row.push(chr);
+                }
             }
         }
         expanded_space.push(expanded_row.clone());
         // push again if the row has only `.`
         if expanded_row.chars().all(|c| c == SPACE) {
-            expanded_space.push(expanded_row);
+            for _ in 0..factor {
+                expanded_space.push(expanded_row.clone());
+            }
         }
     }
 
     expanded_space
 }
 
-pub fn find_galaxies(image: &[String]) -> Vec<(usize, usize)> {
+pub fn expanded_locations(image: &[&str]) -> (Vec<usize>, Vec<usize>) {
+    let mut transposed_image: Vec<String> = vec![];
+
+    let num_col = image
+        .first()
+        .expect("galaxy must contain at least one row")
+        .len();
+
+    let mut col_cnt = 0;
+    while col_cnt < num_col {
+        let mut transposed_row: String = String::new();
+        for line in image.iter() {
+            transposed_row.push(line.chars().nth(col_cnt).expect("column must exist"));
+        }
+        transposed_image.push(transposed_row);
+        col_cnt += 1;
+    }
+
+    let mut expanded_cols: Vec<usize> = vec![];
+    // each value in transposed_galaxy is a character of the `row` in the original
+    for (char_num, col) in transposed_image.iter().enumerate() {
+        if col.chars().all(|c| c == SPACE) {
+            expanded_cols.push(char_num);
+        }
+    }
+
+    let mut expanded_rows: Vec<usize> = vec![];
+    for (row_no, row) in image.iter().enumerate() {
+        if row.chars().all(|c| c == SPACE) {
+            expanded_rows.push(row_no);
+        }
+    }
+
+    (expanded_rows, expanded_cols)
+}
+
+pub fn find_galaxies(image: &[&str]) -> Vec<(usize, usize)> {
     let mut galaxies: Vec<(usize, usize)> = vec![];
     for (row_no, row) in image.iter().enumerate() {
         for (col_no, chr) in row.chars().enumerate() {
@@ -67,11 +105,37 @@ pub fn find_galaxies(image: &[String]) -> Vec<(usize, usize)> {
     galaxies
 }
 
-pub fn find_distance_between_pairs(galaxy_positions: Vec<(usize, usize)>) -> Vec<usize> {
+pub fn find_distance_between_pairs(
+    galaxy_positions: Vec<(usize, usize)>,
+    expanded_spaces: (Vec<usize>, Vec<usize>),
+    expansion_factor: usize,
+) -> Vec<usize> {
     let mut distances: Vec<usize> = vec![];
     for (i, (x, y)) in galaxy_positions.iter().enumerate() {
         for (next_x, next_y) in galaxy_positions[i + 1..].iter() {
-            distances.push(next_x.abs_diff(*x) + next_y.abs_diff(*y));
+            let mut abs_distance = next_x.abs_diff(*x) + next_y.abs_diff(*y);
+            // dbg!(abs_distance);
+
+            let mut num_crossings: usize = 0;
+            for expanded_row in expanded_spaces.0.iter() {
+                if expanded_row.gt(x) && expanded_row.lt(next_x) {
+                    num_crossings += 1;
+                }
+            }
+
+            for expanded_col in expanded_spaces.1.iter() {
+                if (expanded_col.gt(y) && expanded_col.lt(next_y))
+                    || (expanded_col.gt(next_y) && expanded_col.lt(y))
+                {
+                    num_crossings += 1;
+                }
+            }
+            // dbg!(x, y, next_x, next_y, num_crossings);
+
+            abs_distance += (num_crossings * expansion_factor).saturating_sub(num_crossings);
+            // dbg!(abs_distance);
+
+            distances.push(abs_distance);
         }
     }
 
@@ -107,7 +171,7 @@ mod tests {
 .........#...
 #....#.......";
         let image = load_image(input);
-        let expanded_image = expand_space(&image[..]);
+        let expanded_image = expand_space(&image[..], 1);
 
         let mut output: String = String::new();
         for row in expanded_image {
@@ -132,7 +196,7 @@ mod tests {
 .......#..
 #...#.....";
         let image = load_image(input);
-        let expanded_image = expand_space(&image[..]);
+        let expanded_image = expand_space(&image[..], 1);
 
         let expected_galaxies = vec![
             (0, 4),
@@ -145,7 +209,12 @@ mod tests {
             (11, 0),
             (11, 5),
         ];
-        let galaxies = find_galaxies(&expanded_image);
+        let galaxies = find_galaxies(
+            &expanded_image
+                .iter()
+                .map(|s| s.as_str())
+                .collect::<Vec<&str>>(),
+        );
         assert_eq!(galaxies, expected_galaxies);
     }
 
@@ -162,11 +231,12 @@ mod tests {
 .......#..
 #...#.....";
         let image = load_image(input);
-        let expanded_image = expand_space(&image[..]);
-        let galaxies = find_galaxies(&expanded_image);
+        let expanded_locations = expanded_locations(&image);
+        // dbg!(&expanded_locations);
+        let galaxies = find_galaxies(&image);
 
         let expected_sum = 374;
-        let distance_pairs = find_distance_between_pairs(galaxies);
+        let distance_pairs = find_distance_between_pairs(galaxies, expanded_locations, 2);
         assert_eq!(distance_pairs.iter().sum::<usize>(), expected_sum);
     }
 }
--- 2023/rust/day11/src/main.rs
+++ 2023/rust/day11/src/main.rs
@@ -1,20 +1,39 @@
 use std::fs;
 
-use day11::{expand_space, find_distance_between_pairs, find_galaxies, load_image};
+use day11::{expanded_locations, find_distance_between_pairs, find_galaxies, load_image};
 
 const INPUT_DIR: &str = "input";
 
 fn main() {
     part_one(&format!("{INPUT_DIR}/part_one.txt"));
+    part_two(&format!("{INPUT_DIR}/part_two.txt"));
 }
 
 fn part_one(path: &str) {
     let contents = fs::read_to_string(path).expect("Input file must be readable");
+    const EXPANSION_FACTOR: usize = 1;
 
     let image = load_image(&contents);
-    let expanded_image = expand_space(&image[..]);
-    let galaxies = find_galaxies(&expanded_image);
-    let distance_pairs = find_distance_between_pairs(galaxies);
+    let galaxies = find_galaxies(&image);
+    let expanded_locations = expanded_locations(&image[..]);
+    let distance_pairs =
+        find_distance_between_pairs(galaxies, expanded_locations, EXPANSION_FACTOR);
+
+    println!(
+        "Sum of distance between galaxies: {}",
+        distance_pairs.iter().sum::<usize>()
+    );
+}
+
+fn part_two(path: &str) {
+    let contents = fs::read_to_string(path).expect("Input file must be readable");
+    const EXPANSION_FACTOR: usize = 1000000;
+
+    let image = load_image(&contents);
+    let galaxies = find_galaxies(&image);
+    let expanded_locations = expanded_locations(&image);
+    let distance_pairs =
+        find_distance_between_pairs(galaxies, expanded_locations, EXPANSION_FACTOR);
 
     println!(
         "Sum of distance between galaxies: {}",
