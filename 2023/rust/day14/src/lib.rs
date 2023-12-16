8a9fb7acdc1c28a49083704e98c1aa6725135340
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-17 00:13:15 +0545

    Complete 14-02

--- 2023/rust/day14/src/lib.rs
+++ 2023/rust/day14/src/lib.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone)]
+pub const TOTAL_CYCLES: usize = 1000000000;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Rock {
     Round,
     Square,
@@ -13,10 +15,18 @@ impl Rock {
             _ => panic!("unknown object detected: {ch}"),
         }
     }
+
+    pub fn to_char(rock: &Option<Self>) -> char {
+        match rock {
+            Some(Self::Round) => 'O',
+            Some(Self::Square) => '#',
+            None => '.',
+        }
+    }
 }
 
-type RockLine = Vec<Option<Rock>>;
-type RockPattern = Vec<RockLine>;
+pub type RockLine = Vec<Option<Rock>>;
+pub type RockPattern = Vec<RockLine>;
 
 pub fn parse(contents: &str) -> Vec<RockPattern> {
     let mut output: Vec<RockPattern> = vec![];
@@ -82,6 +92,185 @@ pub fn rock_roll_sum(transposed_pattern: &RockPattern) -> usize {
     sum
 }
 
+/// This function cycles the `pattern` repeatedly till the next pattern matches any of the paterns
+/// that have been seen so far.
+///
+/// # Returns
+///
+/// 1. The vector of all patterns that have been explored till seeing a repetition.
+/// 2. The offset point i.e., the index of the pattern that was seen again.
+pub fn cycle_length(pattern: &RockPattern) -> (Vec<RockPattern>, usize) {
+    const MAX_PATTERNS_TO_CHECK: usize = 10000;
+
+    let mut patterns: Vec<RockPattern> = vec![];
+
+    let mut next = spin_cycle(pattern);
+    patterns.push(next.clone());
+
+    loop {
+        let cycle_count = patterns.len();
+
+        next = spin_cycle(&next);
+        for (i, pat) in patterns.iter().enumerate() {
+            if pat.eq(&next) {
+                return (patterns, i);
+            }
+        }
+
+        patterns.push(next.clone());
+
+        if cycle_count > MAX_PATTERNS_TO_CHECK {
+            panic!("Checked {MAX_PATTERNS_TO_CHECK} patterns without any recurrence");
+        }
+    }
+}
+
+pub fn calculate_load_after_cycles(
+    total_cycles: usize,
+    patterns: &[RockPattern],
+    offset: usize,
+) -> usize {
+    let remaining_cycles = total_cycles - offset;
+    let surplus = remaining_cycles % (patterns.len() - offset);
+
+    let index_at_surplus = if surplus == 0 {
+        patterns.len() - 1 // if no surplus, get the last value in the repeating pattern
+    } else {
+        offset + surplus - 1 // if there is a surplus, start counting from the offset point till
+                             // the surplus
+    };
+
+    calculate_load(&patterns[index_at_surplus])
+}
+
+pub fn calculate_load(pattern: &RockPattern) -> usize {
+    let total_height = pattern.len();
+    let mut total_load = 0;
+    for (row, rock_line) in pattern.iter().enumerate() {
+        let round_count: usize = rock_line
+            .iter()
+            .filter_map(|r| {
+                if r.is_some() && r.clone().unwrap() == Rock::Round {
+                    Some(1)
+                } else {
+                    None
+                }
+            })
+            .sum();
+        total_load += round_count * (total_height - row);
+    }
+
+    total_load
+}
+
+pub fn spin_cycle(pattern: &RockPattern) -> RockPattern {
+    roll_east(&roll_south(&roll_west(&roll_north(pattern))))
+}
+
+pub fn roll_north(pat: &RockPattern) -> RockPattern {
+    let height = pat.len();
+    let width = pat[0].len();
+    let mut rolled_pattern: RockPattern = vec![vec![None; width]; height];
+
+    for col in 0..width {
+        let mut rolled_row = 0;
+        for (row, rock_line) in pat.iter().enumerate() {
+            if rock_line[col].is_some() {
+                let element = rock_line[col].clone().unwrap();
+                if element == Rock::Square {
+                    rolled_row = row;
+                }
+                rolled_pattern[rolled_row][col] = Some(element);
+                rolled_row += 1;
+            }
+        }
+    }
+
+    rolled_pattern
+}
+
+pub fn roll_south(pat: &RockPattern) -> RockPattern {
+    let height = pat.len();
+    let width = pat[0].len();
+    let mut rolled_pattern: RockPattern = vec![vec![None; width]; height];
+
+    for col in 0..width {
+        let mut rolled_row = height - 1;
+        for row in (0..height).rev() {
+            if pat[row][col].is_some() {
+                let element = pat[row][col].clone().unwrap();
+                if element == Rock::Square {
+                    rolled_row = row;
+                }
+                rolled_pattern[rolled_row][col] = Some(element);
+                rolled_row = rolled_row.saturating_sub(1);
+            }
+        }
+    }
+
+    rolled_pattern
+}
+
+pub fn roll_west(pat: &RockPattern) -> RockPattern {
+    let height = pat.len();
+    let width = pat[0].len();
+    let mut rolled_pattern: RockPattern = vec![vec![None; width]; height];
+
+    for row in 0..height {
+        let mut rolled_col = 0;
+        for col in 0..width {
+            if pat[row][col].is_some() {
+                let element = pat[row][col].clone().unwrap();
+                if element == Rock::Square {
+                    rolled_col = col;
+                }
+                rolled_pattern[row][rolled_col] = Some(element);
+                rolled_col += 1;
+            }
+        }
+    }
+
+    rolled_pattern
+}
+
+pub fn roll_east(pat: &RockPattern) -> RockPattern {
+    let height = pat.len();
+    let width = pat[0].len();
+    let mut rolled_pattern: RockPattern = vec![vec![None; width]; height];
+
+    for row in 0..height {
+        let mut rolled_col = width - 1;
+        for col in (0..width).rev() {
+            if pat[row][col].is_some() {
+                let element = pat[row][col].clone().unwrap();
+                if element == Rock::Square {
+                    rolled_col = col;
+                }
+                rolled_pattern[row][rolled_col] = Some(element);
+                rolled_col = rolled_col.saturating_sub(1);
+            }
+        }
+    }
+
+    rolled_pattern
+}
+
+pub fn pattern_to_string(pat: &RockPattern) -> String {
+    let mut output = String::new();
+    let last_index = pat.len() - 1;
+    for (i, rock_line) in pat.iter().enumerate() {
+        for rock_type in rock_line {
+            output.push(Rock::to_char(rock_type));
+        }
+
+        if i < last_index {
+            output.push('\n');
+        }
+    }
+
+    output
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -102,5 +291,139 @@ O.#..O.#.#
         let transposed_pattern = transpose(&patterns[0]);
 
         assert_eq!(rock_roll_sum(&transposed_pattern), 136);
+    }
+
+    #[test]
+    fn rolls_north_correctly() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+        let expected: &str = "OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....";
+        let patterns = parse(input);
+        let north_rolled = roll_north(&patterns[0]);
+
+        assert_eq!(pattern_to_string(&north_rolled), expected.to_string());
+    }
+
+    #[test]
+    fn rolls_west_correctly() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+        let expected = "O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#....";
+
+        let patterns = parse(input);
+        let west_rolled = roll_west(&patterns[0]);
+
+        assert_eq!(pattern_to_string(&west_rolled), expected.to_string());
+    }
+
+    #[test]
+    fn rolls_south_correctly() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+        let expected: &str = ".....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O";
+
+        let patterns = parse(input);
+        let south_rolled = roll_south(&patterns[0]);
+
+        assert_eq!(pattern_to_string(&south_rolled), expected.to_string());
+    }
+
+    #[test]
+    fn rolls_east_correctly() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+        let expected: &str = "....O#....
+.OOO#....#
+.....##...
+.OO#....OO
+......OO#.
+.O#...O#.#
+....O#..OO
+.........O
+#....###..
+#..OO#....";
+
+        let patterns = parse(input);
+        let east_rolled = roll_east(&patterns[0]);
+
+        assert_eq!(pattern_to_string(&east_rolled), expected.to_string());
+    }
+
+    #[test]
+    fn finds_cycle_length() {
+        let input = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....";
+
+        let patterns = parse(input);
+        let (min_patterns, offset) = cycle_length(&patterns[0]);
+        let load = calculate_load_after_cycles(TOTAL_CYCLES, &min_patterns, offset);
+        assert_eq!(load, 64);
     }
 }
--- 2023/rust/day14/src/main.rs
+++ 2023/rust/day14/src/main.rs
@@ -1,10 +1,13 @@
 use std::fs;
 
-use day14::{parse, rock_roll_sum, transpose};
+use day14::{
+    calculate_load_after_cycles, cycle_length, parse, rock_roll_sum, transpose, TOTAL_CYCLES,
+};
 
 const INPUT_DIR: &str = "input";
 fn main() {
     part_one(&format!("{INPUT_DIR}/part_one.txt"));
+    part_two(&format!("{INPUT_DIR}/part_two.txt"));
 }
 
 fn part_one(path: &str) {
@@ -21,4 +24,14 @@ fn part_one(path: &str) {
     }
 
     println!("Total load = {total_load}");
+}
+
+fn part_two(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+    let patterns = parse(&contents);
+
+    let (min_patterns, offset) = cycle_length(&patterns[0]);
+    let load = calculate_load_after_cycles(TOTAL_CYCLES, &min_patterns, offset);
+
+    println!("Total load after {TOTAL_CYCLES} cycles = {load}");
 }
