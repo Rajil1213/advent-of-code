d024f8d01cec756ee03e2ce8e0b8a761c6b5f98e
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-17 16:22:02 +0545

    Complete 15-02

--- 2023/rust/day15/src/lib.rs
+++ 2023/rust/day15/src/lib.rs
@@ -1,71 +1,143 @@
-use std::hash::{BuildHasher, Hasher};
+use std::str::FromStr;
 
-#[derive(Default)]
-pub struct AsciiHasher {
-    state: u64,
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Label(String);
+
+impl FromStr for Label {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Label(s.to_string()))
+    }
 }
 
-impl Hasher for AsciiHasher {
-    fn write(&mut self, bytes: &[u8]) {
-        let mut current_value: u64 = 0;
-        for chr in bytes {
-            current_value += u64::from(*chr);
+impl Label {
+    pub fn hash(&self) -> usize {
+        let mut current_value: usize = 0;
+        for chr in self.0.as_bytes() {
+            current_value += usize::from(*chr);
             current_value *= 17;
             current_value %= 256;
         }
-        self.state = current_value;
-    }
 
-    fn finish(&self) -> u64 {
-        self.state
+        current_value
     }
 }
 
-pub struct AocAsciiHasherBuilder;
-
-impl BuildHasher for AocAsciiHasherBuilder {
-    type Hasher = AsciiHasher;
-
-    fn build_hasher(&self) -> Self::Hasher {
-        AsciiHasher { state: 0 }
-    }
-}
-
-pub fn sum_hashes(contents: &str) -> u64 {
-    let mut sum: u64 = 0;
+pub fn sum_hashes(contents: &str) -> usize {
+    let mut sum: usize = 0;
 
     for value in contents.split(',') {
-        let mut hasher = AsciiHasher::default();
-        let value_bytes = value.strip_suffix('\n').unwrap_or(value).as_bytes();
-        hasher.write(value_bytes);
-        let hashed_value = hasher.finish();
-        println!(
-            "{:?} => {hashed_value}",
-            String::from_utf8(value_bytes.to_vec())
-        );
+        let label = Label::from_str(value.strip_suffix('\n').unwrap_or(value)).unwrap();
+        let hashed_value = label.hash();
+
         sum += hashed_value;
     }
 
     sum
 }
 
+const MINUS: char = '-';
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Lens {
+    label: Label,
+    focal_length: u8,
+}
+
+pub fn sum_focusing_power(boxes: &[Vec<Lens>]) -> usize {
+    let mut sum = 0;
+    for (box_num, v) in boxes.iter().enumerate() {
+        // println!("{k:?} => {box_num}");
+
+        for (i, lens) in v.iter().enumerate() {
+            sum += (box_num + 1) * (i + 1) * usize::from(lens.focal_length);
+        }
+    }
+
+    sum
+}
+
+pub fn form_boxes(contents: &str) -> Vec<Vec<Lens>> {
+    let mut boxes: Vec<Vec<Lens>> = vec![vec![]; 256];
+    let contents = contents.trim_end();
+    for step in contents.split(',') {
+        if step.ends_with(MINUS) {
+            let label = Label::from_str(step.strip_suffix(MINUS).unwrap()).unwrap();
+            let box_num = label.hash();
+            if !boxes[box_num].is_empty() {
+                boxes[box_num] = boxes[box_num]
+                    .iter()
+                    .filter_map(|lens| {
+                        if lens.label == label {
+                            return None;
+                        }
+                        Some(lens.clone())
+                    })
+                    .collect::<Vec<Lens>>()
+                    .to_vec();
+            }
+        } else {
+            // <label>=(1..=9)
+            let label = Label::from_str(&step[..step.len() - 2]).unwrap();
+            let focal_length = step
+                .chars()
+                .nth(step.len() - 1)
+                .unwrap()
+                .to_string()
+                .parse::<u8>()
+                .unwrap();
+            let box_num = label.hash();
+
+            if boxes[box_num].is_empty() {
+                boxes[box_num].push(Lens {
+                    label,
+                    focal_length,
+                });
+            } else {
+                let mut exists = false;
+                boxes[box_num] = boxes[box_num]
+                    .iter_mut()
+                    .map(|l| {
+                        if l.label == label {
+                            l.focal_length = focal_length;
+                            exists = true;
+                        }
+                        l.clone()
+                    })
+                    .collect::<Vec<Lens>>();
+
+                if !exists {
+                    boxes[box_num].push(Lens {
+                        label,
+                        focal_length,
+                    });
+                }
+            }
+        }
+    }
+
+    boxes
+}
+
 #[cfg(test)]
 mod tests {
-    use std::hash::Hasher;
-
     use super::*;
     use rstest::rstest;
 
     #[rstest]
-    #[case(b"rn=1", 30)]
-    #[case(b"cm-", 253)]
-    #[case(b"qp=3", 97)]
-    #[case(b"ot=9", 9)]
-    fn hashes_correctly(#[case] input: &[u8], #[case] expected: u64) {
-        let mut hasher = AsciiHasher::default();
-        hasher.write(input);
+    #[case("rn=1", 30)]
+    #[case("cm-", 253)]
+    #[case("qp=3", 97)]
+    #[case("ot=9", 9)]
+    #[case("ot", 3)]
+    #[case("ab", 3)]
+    #[case("rn", 0)]
+    #[case("cm", 0)]
+    fn hashes_correctly(#[case] input: &str, #[case] expected: usize) {
+        let label = Label::from_str(input).unwrap();
 
-        assert_eq!(hasher.finish(), expected);
+        assert_eq!(label.hash(), expected);
     }
 
     #[test]
@@ -74,5 +146,16 @@ mod tests {
         let sum = sum_hashes(input);
 
         assert_eq!(sum, 1320);
+    }
+
+    #[test]
+    fn calculates_focusing_power_correctly() {
+        let input: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+        let boxes = form_boxes(input);
+        dbg!(&boxes);
+
+        let focussing_power = sum_focusing_power(&boxes);
+
+        assert_eq!(focussing_power, 145);
     }
 }
--- 2023/rust/day15/src/main.rs
+++ 2023/rust/day15/src/main.rs
@@ -1,10 +1,11 @@
 use std::fs;
 
-use day15::sum_hashes;
+use day15::{form_boxes, sum_focusing_power, sum_hashes};
 
 const INPUT_DIR: &str = "input";
 fn main() {
     part_one(&format!("{INPUT_DIR}/part_one.txt"));
+    part_two(&format!("{INPUT_DIR}/part_two.txt"));
 }
 
 fn part_one(path: &str) {
@@ -13,4 +14,13 @@ fn part_one(path: &str) {
     let sum = sum_hashes(&contents);
 
     println!("Sum of hashes = {sum}");
+}
+
+fn part_two(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+
+    let boxes = form_boxes(&contents);
+    let focussing_power = sum_focusing_power(&boxes);
+
+    println!("Total focussing power = {focussing_power}");
 }
