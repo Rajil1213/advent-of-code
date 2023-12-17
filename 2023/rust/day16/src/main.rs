209156fc3690c225bf53125f0df99ac79af86521
Rajil Bajracharya <imrb1213@gmail.com>
2023-12-17 22:30:09 +0545

    Complete 16-02

--- 2023/rust/day16/src/main.rs
+++ 2023/rust/day16/src/main.rs
@@ -6,6 +6,7 @@ const INPUT_DIR: &str = "input";
 
 fn main() {
     part_one(&format!("{INPUT_DIR}/part_one.txt"));
+    part_two(&format!("{INPUT_DIR}/part_one.txt"));
 }
 
 fn part_one(path: &str) {
@@ -17,4 +18,59 @@ fn part_one(path: &str) {
     let count = count_energized(&grid);
 
     println!("Energized grids = {count}");
+}
+
+fn part_two(path: &str) {
+    let contents = fs::read_to_string(path).expect("input file must be readable");
+
+    let original_grid = parse(&contents);
+    let width = original_grid.len();
+
+    let mut maximum = 0;
+
+    // check all on the top row:
+    for i in 0..width {
+        let mut current_grid = original_grid.clone();
+        traverse_grid((0, i), &Direction::Down, &mut current_grid);
+
+        let count = count_energized(&current_grid);
+        if maximum < count {
+            maximum = count;
+        }
+    }
+
+    // check all on the bottom row:
+    for i in 0..width {
+        let mut current_grid = original_grid.clone();
+        traverse_grid((width - 1, i), &Direction::Up, &mut current_grid);
+
+        let count = count_energized(&current_grid);
+        if maximum < count {
+            maximum = count;
+        }
+    }
+
+    // check all on the left column:
+    for i in 0..width {
+        let mut current_grid = original_grid.clone();
+        traverse_grid((i, 0), &Direction::Right, &mut current_grid);
+
+        let count = count_energized(&current_grid);
+        if maximum < count {
+            maximum = count;
+        }
+    }
+
+    // check all on the right column:
+    for i in 0..width {
+        let mut current_grid = original_grid.clone();
+        traverse_grid((i, width - 1), &Direction::Left, &mut current_grid);
+
+        let count = count_energized(&current_grid);
+        if maximum < count {
+            maximum = count;
+        }
+    }
+
+    println!("Maximum possible energized beams = {maximum}");
 }
