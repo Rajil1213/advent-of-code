03e2b933db603e65c16a80ae65261fb0f8e950e6
Rajil Bajracharya <imrb1213@gmail.com>
2024-12-02 17:06:05 +0545

    chore: update template

--- 2024/haskell/src/Days/Day14.hs
+++ 2024/haskell/src/Days/Day14.hs
@@ -1,6 +1,6 @@
-module Days.Day14 (runDay) where
+module Days.Day14 (runDay, partA, partB, inputParser, lineParser) where
 
-import Data.List
+import Data.List (sort)
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
 import Data.Maybe
@@ -9,6 +9,7 @@ import Data.Set qualified as Set
 import Data.Vector (Vector)
 import Data.Vector qualified as Vec
 
+import Control.Applicative ((<|>))
 import Data.Attoparsec.Text
 import Data.Void
 import Program.RunDay qualified as R (Day, runDay)
@@ -18,19 +19,37 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  pairs <- many' lineParser
+  return (map fst pairs, map snd pairs)
+
+lineParser :: Parser (Int, Int)
+lineParser = do
+  a <- decimal
+  skipSpace
+  b <- decimal
+  endOfLine <|> endOfInput
+  return (a, b)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = ([Int], [Int])
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA pair = sum . map abs $ zipWith (-) (sort firstList) (sort secondList)
+ where
+  firstList = fst pair
+  secondList = snd pair
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB pair = sum score
+ where
+  score = [x * x `countElem` snd pair | x <- fst pair]
+
+countElem :: Int -> [Int] -> Int
+countElem x lst = length [a | a <- lst, x == a]
