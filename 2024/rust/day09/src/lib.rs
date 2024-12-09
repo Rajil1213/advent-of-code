9977ae19fe675d34cc129df9f4d700708f50f23b
Rajil Bajracharya <imrb1213@gmail.com>
2024-12-09 13:20:47 +0545

    Complete 2024-09 part 2 (rust)

--- 2024/rust/day09/src/lib.rs
+++ 2024/rust/day09/src/lib.rs
@@ -3,11 +3,17 @@ use std::collections::BTreeMap;
 pub type Files = BTreeMap<usize, usize>;
 pub type Spaces = Vec<usize>;
 
-pub fn parse(input: &str) -> (Files, Spaces) {
+pub type SpaceBlocks = BTreeMap<usize, usize>; // starting position -> count
+pub type FileBlocks = BTreeMap<usize, (usize, usize)>; // starting_position -> (file_id, count)
+
+pub fn parse(input: &str) -> (Files, Spaces, FileBlocks, SpaceBlocks) {
     let line = input.lines().next().expect("must have at least one line");
 
     let mut files: Files = BTreeMap::new();
     let mut spaces: Spaces = vec![];
+
+    let mut file_blocks: FileBlocks = BTreeMap::new();
+    let mut space_blocks: SpaceBlocks = BTreeMap::new();
 
     let mut position = 0;
     let mut file_id = 0;
@@ -24,16 +30,19 @@ pub fn parse(input: &str) -> (Files, Spaces) {
         }
 
         if is_file {
+            file_blocks.insert(position, (file_id, num_elems));
             file_id += 1;
+        } else {
+            space_blocks.insert(position, num_elems);
         }
 
         position += num_elems;
     }
 
-    (files, spaces)
+    (files, spaces, file_blocks, space_blocks)
 }
 
-pub fn part_one(files: &BTreeMap<usize, usize>, spaces: &[usize]) -> usize {
+pub fn part_one(files: &Files, spaces: &Spaces) -> usize {
     let mut contiguous_files: Files = files.clone();
 
     for ((file_position, file_id), space_position) in files.iter().rev().zip(spaces) {
@@ -47,14 +56,75 @@ pub fn part_one(files: &BTreeMap<usize, usize>, spaces: &[usize]) -> usize {
 
     let mut checksum = 0;
     for (file_position, file_id) in contiguous_files {
-        print!("{file_id}");
         checksum += file_position * file_id
     }
 
-    println!();
+    checksum
+}
+
+pub fn part_two(file_blocks: &FileBlocks, space_blocks: &SpaceBlocks) -> usize {
+    let mut consolidated_files: FileBlocks = file_blocks.clone();
+
+    let mut space_counts = space_blocks.iter();
+
+    loop {
+        let space_counts = space_counts.next();
+        if space_counts.is_none() {
+            break;
+        }
+
+        let (space_position, space_to_fill) = space_counts.unwrap();
+        let (mut space_position, mut space_to_fill) = (*space_position, *space_to_fill);
+
+        let updated_consolidated_files = consolidated_files
+            .iter()
+            .filter_map(|(file_pos, value)| {
+                if *file_pos > space_position {
+                    Some((*file_pos, *value))
+                } else {
+                    None
+                }
+            })
+            .collect::<FileBlocks>();
+
+        if updated_consolidated_files.is_empty() {
+            break;
+        }
+
+        for (file_position, (file_id, file_count)) in updated_consolidated_files.into_iter().rev() {
+            if file_position < space_position {
+                break;
+            }
+
+            if file_count == space_to_fill {
+                consolidated_files.insert(space_position, (file_id, file_count));
+                consolidated_files.remove(&file_position);
+
+                break;
+            }
+
+            if file_count < space_to_fill {
+                consolidated_files.insert(space_position, (file_id, file_count));
+                consolidated_files.remove(&file_position);
+
+                space_to_fill -= file_count;
+                space_position += file_count;
+            }
+        }
+    }
+
+    let mut checksum = 0;
+    for (file_position, (file_id, file_count)) in consolidated_files {
+        for i in 0..file_count {
+            checksum += (file_position + i) * file_id;
+        }
+    }
 
     checksum
 }
+
+pub type SpaceBlock = BTreeMap<usize, usize>; // starting position -> count
+pub type FileBlock = BTreeMap<usize, (usize, usize)>; // starting_position -> (file_id, count)
 
 #[cfg(test)]
 mod tests {
@@ -77,19 +147,35 @@ mod tests {
         ]);
         let expected_spaces = vec![1, 2, 6, 7, 8, 9];
 
-        let (actual_files, actual_spaces) = parse(input);
+        let expected_file_blocks = BTreeMap::from([(0, (0, 1)), (3, (1, 3)), (10, (2, 5))]);
+        let expected_space_blocks = BTreeMap::from([(1, 2), (6, 4)]);
+
+        let (actual_files, actual_spaces, actual_file_blocks, actual_space_blocks) = parse(input);
 
         assert_eq!(actual_files, expected_files);
         assert_eq!(actual_spaces, expected_spaces);
+
+        assert_eq!(actual_file_blocks, expected_file_blocks);
+        assert_eq!(actual_space_blocks, expected_space_blocks);
     }
 
     #[rstest]
     #[case("12345", 60)]
     #[case("2333133121414131402", 1928)]
     fn test_part_one(#[case] input: &str, #[case] expected: usize) {
-        let (files, spaces) = parse(input);
+        let (files, spaces, _, _) = parse(input);
 
         let actual = part_one(&files, &spaces);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[rstest]
+    #[case("2333133121414131402", 2858)]
+    fn test_part_two(#[case] input: &str, #[case] expected: usize) {
+        let (_, _, file_blocks, space_blocks) = parse(input);
+
+        let actual = part_two(&file_blocks, &space_blocks);
 
         assert_eq!(actual, expected);
     }
--- 2024/rust/day09/src/main.rs
+++ 2024/rust/day09/src/main.rs
@@ -1,13 +1,16 @@
 use std::fs;
 
-use day09::{parse, part_one};
+use day09::{parse, part_one, part_two};
 
 const INPUT_FILE: &str = "input/day09.txt";
 
 fn main() {
     let input = fs::read_to_string(INPUT_FILE).expect("input file must exist");
-    let (files, spaces) = parse(&input);
+    let (files, spaces, file_blocks, space_blocks) = parse(&input);
 
     let part_one_result = part_one(&files, &spaces);
     println!("Part 1 Solution: {part_one_result}");
+
+    let part_two_result = part_two(&file_blocks, &space_blocks);
+    println!("Part 2 Solution: {part_two_result}");
 }
